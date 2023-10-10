44cac40d591f6d1b1b59d85699113899569b2c66
Tofunmi Odusile <tofunmi.odusile@kibo.school>
2023-10-10 05:09:36 +0100

    .

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/api/data', methods=['GET'])
+def api_data():
+    data = {'name': 'John', 'age': 30}
+    return jsonify(data)
